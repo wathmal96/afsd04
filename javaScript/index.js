63876215c6fd727c06645fa563a70c7309145794
wathmal96 <ranaweera1125vinura@gmail.com>
2024-03-03 13:32:35 +0530

    add h1

---
+++ javaScript/index.js
@@ -0,0 +1,18 @@
+console.log("I live in moratuwa")
+
+document.write("wathmal bandara")
+document.write("<h1>truth</h1>")
+// alert("violant")
+
+{let x = 10;
+    x=20
+}
+
+{let x = 5}
+let x=39
+
+console.log(x);
+
+// const array = [1,2,3,4]
+// array = [1,2,3,4]
+// console.log(array)
